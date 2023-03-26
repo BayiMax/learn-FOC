f58afd472a2f88c9dc18e4e216bb4e47826554d3
baymax <2978043737@qq.com>
2023-03-26 16:03:25 +0800

    add code

---
+++ project/foc-code/main/main.c
@@ -0,0 +1,83 @@
+/*
+ *                        _oo0oo_
+ *                       o8888888o
+ *                       88" . "88
+ *                       (| -_- |)
+ *                       0\  =  /0
+ *                     ___/`---'\___
+ *                   .' \\|     |// '.
+ *                  / \\|||  :  |||// \
+ *                 / _||||| -:- |||||- \
+ *                |   | \\\  - /// |   |
+ *                | \_|  ''\---/''  |_/ |
+ *                \  .-\__  '-'  ___/-. /
+ *              ___'. .'  /--.--\  `. .'___
+ *           ."" '<  `.___\_<|>_/___.' >' "".
+ *          | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *          \  \ `_.   \_ __\ /__ _/   .-` /  /
+ *      =====`-.____`.___ \_____/___.-`___.-'=====
+ *                        `=---='
+ *
+ *
+ *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *            佛祖保佑     永不宕机     永无BUG
+ */
+
+/**
+ * @file app_main.c
+ * @author {白秉鑫}-{bbx20010518@outlook.com}
+ * @brief
+ * @version 0.1
+ * @date 2023-03-26
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include "main.h"
+
+void app_main(void)
+{
+    int free_size;
+
+    static char InfoBuffer[512] = {0};
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    for (int i = 10; i >= 0; i--)
+    {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+
+    /*user*/
+    foc_init();
+    /*user*/
+    while (1)
+    {
+        vTaskDelay(2000 / portTICK_RATE_MS);
+
+        free_size = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
+        printf("内部RAM剩余%d.%dKB  ...", free_size / 1024, free_size % 1024);
+        printf("芯片可用内存:%fKB\n", ((float)esp_get_free_heap_size() / 1024));
+        vTaskList((char *)&InfoBuffer);
+        printf("任务名      任务状态 优先级   剩余栈 任务序号\r\n");
+        printf("\r\n%s\r\n", InfoBuffer);
+    }
+}
